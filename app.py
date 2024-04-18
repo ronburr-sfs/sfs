2ba62cca39d5260a708ac2f913cc5be3370fa183
Ron Burr <ronfburr@gmail.com>
2024-04-18 17:02:33 -0400

    Various page routes and templates updates especially under MSX.

--- app.py
+++ app.py
@@ -45,3 +45,51 @@ def ron_burr():
 @app.route('/ronburr')
 def ronburr():
     return render_template('ron_burr.html')
+
+@app.route('/msx')
+def msx():
+    return render_template('msx_home.html')
+
+@app.route('/msx-products-compressors')
+def msx_products_compressors():
+    return render_template('msx_products_compressors.html')
+
+@app.route('/msx-products-controllers')
+def msx_products_controllers():
+    return render_template('msx_products_controllers.html')
+
+@app.route('/msx-products-linear-motors')
+def msx_products_linear_motors():
+    return render_template('msx_products_linear_motors.html')
+
+@app.route('/msx-products-resonators')
+def msx_products_resonators():
+    return render_template('msx_products_resonators.html')
+
+@app.route('/msx-products')
+def msx_products():
+    return render_template('msx_products.html')
+
+@app.route('/msx-services')
+def msx_services():
+    return render_template('msx_services.html')
+
+@app.route('/msx-sonic-mixers-benefits')
+def msx_sonic_mixers_benefits():
+    return render_template('msx_sonic_mixers_benefits.html')
+
+@app.route('/msx-sonic-mixers-performance')
+def msx_sonic_mixers_performance():
+    return render_template('msx_sonic_mixers_performance.html')
+
+@app.route('/msx-sonic-mixers-products')
+def msx_sonic_mixers_products():
+    return render_template('msx_sonic_mixers_products.html')
+
+@app.route('/msx-sonic-mixers-video')
+def msx_sonic_mixers_video():
+    return render_template('msx_sonic_mixers_video.html')
+
+@app.route('/msx-sonic-mixers')
+def msx_sonic_mixers():
+    return render_template('msx_sonic_mixers.html')
