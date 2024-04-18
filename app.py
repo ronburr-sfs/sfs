267486ea4dc2baba2380d533463d72b7cc2b99f7
Ron Burr <ronfburr@gmail.com>
2024-04-18 05:54:59 -0400

    Add templates and static folders.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello Sammy!2'
+def hello():
+    return render_template('index.html')
